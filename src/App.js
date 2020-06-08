522c90e99f64cf4b50aea0a31740c229ad1cb9a5
Jonathan Dorety <jdorety@flur.ee>
2020-06-08 16:33:44 -0700

    Add router and paths to auth forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import AuthForm from "./components/Authentication/AuthForm";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <AuthForm register/>
-      {/* <LoginForm /> */}
+      <Switch>
+        <Route path="/register">
+          <AuthForm register />
+        </Route>
+        <Route path="/login">
+          <AuthForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
