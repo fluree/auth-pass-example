09e5262d4811c7639147327eef566b57cb9aebe5
Jonathan Dorety <jdorety@flur.ee>
2020-06-09 23:20:40 -0700

    Configure axios Authorization header

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const url = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const token = localStorage.getItem("authExample") || "";
 const instance = axios.create({
-  baseURL: url
-})
+  baseURL: url,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
 
 export default instance;
