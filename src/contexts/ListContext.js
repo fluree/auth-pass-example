f1bcae9dce2a8d62b23dec3bd0396e888f528741
Jonathan Dorety <jdorety@flur.ee>
2020-06-22 14:34:58 -0400

    Create context provider for pull lists

--- src/contexts/ListContext.js
+++ src/contexts/ListContext.js
@@ -1,5 +1,32 @@
 import { createContext } from "react";
+import { axiosBase } from "../utils/axios";
 
 export const ListContext = createContext({
+  pullList: [],
+  add: (book, userId) => {
+    const transaction = [
+      {
+        _id: userId,
+        pull_list: [book._id],
+      },
+    ];
+    axiosBase
+      .post("/fdb/example/comics/transact", transaction)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  },
+  get: (user) => {
+    const query = {
+      select: ["pull_list"],
+      from: user,
+    };
+    axiosBase
+      .post("/fdb/example/comics/query", query)
+      .then((res) => {
+        console.log("in the list", res);
+      })
+      .catch((err) => console.log(err));
+  },
 });
-
