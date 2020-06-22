df74c336fadff528a313e4d5c5f9cbc99030293b
Jonathan Dorety <jdorety@flur.ee>
2020-06-22 14:55:03 -0400

    Add logout button

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  rightButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
 function NavBar() {
+  const classes = useStyles();
   const history = useHistory();
 
   const clickHandler = (url) => {
     history.push(url);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authExample");
+    history.push("/login");
   };
 
   return (
@@ -17,6 +30,13 @@ function NavBar() {
         </Button>
         <Button color="inherit" onClick={() => clickHandler("/list")}>
           List
+        </Button>
+        <Button
+          color="inherit"
+          className={classes.rightButton}
+          onClick={logout}
+        >
+          Logout
         </Button>
       </Toolbar>
     </AppBar>
