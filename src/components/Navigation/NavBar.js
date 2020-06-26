744cdc3a53c6ad9048ea54262b11788ccf85a353
Jonathan Dorety <jdorety@flur.ee>
2020-06-26 14:30:37 -0400

    Refactor w/ global logout, reroute home button

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { UserContext } from "../../contexts/UserContext";
 
 const useStyles = makeStyles((theme) => ({
   rightButton: {
@@ -9,32 +10,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function NavBar() {
+function NavBar(props) {
   const classes = useStyles();
   const history = useHistory();
+  const user = useContext(UserContext);
 
   const clickHandler = (url) => {
     history.push(url);
   };
 
-  const logout = () => {
-    localStorage.removeItem("authExample");
-    history.push("/login");
-  };
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Button color="inherit" onClick={() => clickHandler("/")}>
+        <Button color="inherit" onClick={() => clickHandler("/books")}>
           Home
         </Button>
         <Button color="inherit" onClick={() => clickHandler("/list")}>
           List
         </Button>
+        {user.role === "employee" && (
+          <Button color="inherit" onClick={() => clickHandler("/adminlist")}>Admin List</Button>
+        )}
         <Button
           color="inherit"
           className={classes.rightButton}
-          onClick={logout}
+          onClick={props.logout}
         >
           Logout
         </Button>
