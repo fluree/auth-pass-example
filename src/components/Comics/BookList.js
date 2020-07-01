1b8129fe28ef2fa59e25ea5596833c605c754726
Jonathan Dorety <jdorety@flur.ee>
2020-07-01 11:09:13 -0400

    Refactor to use fluree helper functions

--- src/components/Comics/BookList.js
+++ src/components/Comics/BookList.js
@@ -3,14 +3,14 @@ import {
   Container,
   List,
   ListItem,
-  ListItemText,
   ListItemIcon,
   Button,
+  CircularProgress,
 } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import Book from "./Book";
 import { UserContext } from "../../contexts/UserContext";
-import { axiosBase } from "../../utils/axios";
+import { flureeQuery, flureeTransact } from "src/utils/flureeFunctions";
 
 function BookList() {
   const [books, setBooks] = useState([]);
@@ -25,11 +25,16 @@ function BookList() {
       },
     ];
     console.log(transaction);
-    axiosBase
-      .post("/fdb/example/comics/transact", transaction)
+    flureeTransact(transaction)
       .then((res) => {
         console.log(res);
       })
+      .catch((err) => console.log(err));
+  };
+
+  const getBooks = (query) => {
+    return flureeQuery(query)
+      .then((comics) => comics)
       .catch((err) => console.log(err));
   };
 
@@ -41,20 +46,14 @@ function BookList() {
         compact: true,
       },
     };
-    axiosBase
-      .post("/fdb/example/comics/query", query)
-      .then((res) => {
-        console.log(res);
-        const comics = res.data;
-        setBooks(comics);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    getBooks(query).then((comics) => {
+      setBooks(comics);
+    });
   }, []);
 
   return (
     <Container>
+      (
       <List>
         {books.map((book, index) => {
           return (
